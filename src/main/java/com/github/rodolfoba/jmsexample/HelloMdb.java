946b95c1fe9b2a82f92dfa908fb8d8e34064221a
rodolfoba <rodolfoba87@gmail.com>
2019-06-13 23:32:36 -0300

    Converting to Wildfly 17

--- src/main/java/com/github/rodolfoba/jmsexample/HelloMdb.java
+++ src/main/java/com/github/rodolfoba/jmsexample/HelloMdb.java
@@ -1,20 +1,19 @@
 package com.github.rodolfoba.jmsexample;
 
-import java.util.logging.Logger;
-
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import java.util.logging.Logger;
 
-@MessageDriven(name = "ExampleMDB", activationConfig = {
+@MessageDriven(name = "HelloMdb", activationConfig = {
         @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "queue/DLQ"),
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
         @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
-public class ExampleMDB implements MessageListener {
+public class HelloMdb implements MessageListener {
 
-    private static final Logger logger = Logger.getLogger(ExampleMDB.class.toString());
+    private static final Logger logger = Logger.getLogger(HelloMdb.class.toString());
 
     @Override
     public void onMessage(Message message) {
